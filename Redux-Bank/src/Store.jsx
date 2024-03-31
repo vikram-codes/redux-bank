a19dc48ff61cbe7e81d381720fcce1a5ebd96141
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-31 18:49:10 +0530

    Installed React Redux

--- Redux-Bank/src/Store.jsx
+++ Redux-Bank/src/Store.jsx
@@ -4,13 +4,6 @@ import { createStore, combineReducers } from "redux";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
 
-// function Store() {
-//   const [{ balance, loan, loanPurpose }, dispatch] = useReducer(
-//     reducer,
-//     initialState
-//   );
-// }
-
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
@@ -18,6 +11,4 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-// store.dispatch({ type: "account/deposit", payload: 500 });
-
-console.log(store.getState());
+export default store;
